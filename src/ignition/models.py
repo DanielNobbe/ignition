26699920b2642316496c459c919f8e8cdce415be
Daniël Nobbe <daniellnobbe@gmail.com>
2025-08-04 17:42:26 +0200

    configurable models

--- src/ignition/models.py
+++ src/ignition/models.py
@@ -1,5 +1,19 @@
-from torchvision.models.segmentation import deeplabv3_resnet50
+from torchvision.models.segmentation import deeplabv3_resnet50, deeplabv3_mobilenet_v3_large
 
 
 def setup_model(config):
-    return deeplabv3_resnet50(num_classes=config.num_classes)
+    """Setup model based on the configuration."""
+    if config.model.name == "deeplabv3_mobilenet_v3_large":
+        if config.model.pretrained:
+            return deeplabv3_mobilenet_v3_large(
+                num_classes=config.num_classes, weights="DEFAULT", weights_backbone="DEFAULT"
+            )
+        else:
+            return deeplabv3_mobilenet_v3_large(
+                num_classes=config.num_classes, weights=None, weights_backbone=None
+            )
+    elif config.model.name == "deeplabv3_resnet50":
+        if config.model.pretrained:
+            return deeplabv3_resnet50(num_classes=config.num_classes, weights="DEFAULT")
+        else:
+            return deeplabv3_resnet50(num_classes=config.num_classes, weights=None)
