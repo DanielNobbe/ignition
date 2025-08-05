e7b37fc0378554a62e4ad20acd3e49e059c77031
Daniël Nobbe <daniellnobbe@gmail.com>
2025-08-05 14:57:46 +0200

    integrate generic dataset into trainer

--- src/ignition/trainers.py
+++ src/ignition/trainers.py
@@ -1,15 +1,14 @@
-from typing import Any, Dict, Union
+from typing import Any, Dict, Union, Callable
 
 import ignite.distributed as idist
 import torch
-from .data import prepare_image_mask
 from ignite.engine import DeterministicEngine, Engine, Events, create_supervised_trainer, create_supervised_evaluator
 from ignite.metrics import Metric
 from torch.cuda.amp import autocast, GradScaler
 from torch.nn import Module
 from torch.optim import Optimizer
 from torch.utils.data import DistributedSampler, Sampler
-from .utils import model_output_transform, model_eval_output_transform, model_train_output_transform
+from ignition.models import IgnitionModel
 
 import logging
 
@@ -17,13 +16,13 @@ logger = logging.getLogger(__name__)
 
 def setup_trainer(
     config: Any,
-    model: Module,
+    model: IgnitionModel,
     optimizer: Optimizer,
     loss_fn: Module,
     device: Union[str, torch.device],
+    prepare_batch: Callable,
     metrics: Dict[str, Metric] = None,
 ) -> Union[Engine, DeterministicEngine]:
-    prepare_batch = prepare_image_mask
 
     if config.use_amp:
         scaler = GradScaler(enabled=config.use_amp)
@@ -39,8 +38,8 @@ def setup_trainer(
         deterministic=True,
         non_blocking=True,
         scaler=scaler,
-        model_transform=model_output_transform,
-        output_transform=model_train_output_transform
+        model_transform=model.get_model_transform(),
+        output_transform=model.get_train_output_transform()
     )
 
     for name, metric in (metrics or {}).items():
@@ -51,11 +50,13 @@ def setup_trainer(
 
 def setup_evaluator(
     config: Any,
-    model: Module,
+    model: IgnitionModel,
     metrics: Dict[str, Metric],
     device: Union[str, torch.device],
+    prepare_batch: Callable = None,
 ) -> Engine:
-    prepare_batch = prepare_image_mask
+
+# TODO: Move metrics declaration into here?
 
     evaluator = create_supervised_evaluator(
         model,
@@ -63,7 +64,7 @@ def setup_evaluator(
         device=device,
         non_blocking=True,
         prepare_batch=prepare_batch,
-        output_transform=model_eval_output_transform,
+        output_transform=model.get_eval_output_transform(),
     )
 
     for name, metric in metrics.items():
