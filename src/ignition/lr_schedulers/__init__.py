1714a4c6147170580fdf839c49edfcceb6d8bb13
Daniël Nobbe <daniellnobbe@gmail.com>
2025-08-29 14:29:20 +0200

    instantiate scheduler and optimizer
    Signed-off-by: Daniël Nobbe <daniellnobbe@gmail.com>

--- src/ignition/lr_schedulers/__init__.py
+++ src/ignition/lr_schedulers/__init__.py
@@ -1,6 +1,32 @@
 from hydra.utils import instantiate
 from torch.optim.lr_scheduler import PolynomialLR
 
+from omegaconf import DictConfig, ListConfig
+
+from torch.optim import Optimizer
+
+from typing import Any
+
+
+def instantiate_scheduler(config: DictConfig, optimizer: Optimizer, **kwargs) -> Any:
+    """Instantiate a handler from the configuration."""
+    handler_requires_arg = config.pop("_requires_", None)
+
+    if handler_requires_arg is not None:
+        if not isinstance(handler_requires_arg, ListConfig | list):
+            handler_requires_arg = [handler_requires_arg]
+
+        # If the handler requires some parameters, instantiate it with them
+        required_args = {arg: kwargs[arg] for arg in handler_requires_arg}
+        return instantiate(config, optimizer=optimizer, _convert_="all", **required_args)
+        # _convert_="all" is needed here to ensure that the required args are all passed as their inherent types.
+        # This is needed because ignite's tree_apply function does not handle dictconfig objects as dicts..
+    else:
+        # Otherwise, instantiate it without any parameters
+        breakpoint()
+        return instantiate(config, optimizer=optimizer)
+
+
 
 def setup_lr_scheduler(optimizer, config, dataset_length):
     """
@@ -8,7 +34,7 @@ def setup_lr_scheduler(optimizer, config, dataset_length):
     """
     if config.lr_scheduler.get("_target_", False):
         # If the config is a full path to a class, instantiate it
-        return instantiate(config.lr_scheduler, optimizer=optimizer, dataset_length=dataset_length)
+        return instantiate_scheduler(config.lr_scheduler, optimizer=optimizer)
     elif config.lr_scheduler.type == "PolynomialLR":
         return PolynomialLR(optimizer, power=config.lr_scheduler.power, total_iters=config.max_epochs * dataset_length)
     else:
