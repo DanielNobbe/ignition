08c8339699248e7875c3fffd9da49b730981a949
Daniël Nobbe <daniellnobbe@gmail.com>
2025-10-30 12:06:39 +0100

    implement report-it paired dataset
    Signed-off-by: Daniël Nobbe <daniellnobbe@gmail.com>

--- src/ignition/datasets/__init__.py
+++ src/ignition/datasets/__init__.py
@@ -1,10 +1,12 @@
 from .base import PairedDataset, IgnitionDataset
 from .monai import SegmentationFolder, EvalSegmentationFolder
 from .vocsegmentation import VOCSegmentationPairedDataset
+from .ri import RiDatasetFromFile
 
 
 # TODO: Move to hydra instantiate
 def setup_dataset(config):
+    
     match config.dataset.type:
         case "VOCSegmentationPairedDataset":
             return VOCSegmentationPairedDataset(config=config)
@@ -12,6 +14,8 @@ def setup_dataset(config):
             return SegmentationFolder(config=config)
         case "MonaiEvalSegmentationFolder":
             return EvalSegmentationFolder(config=config)
+        case "RiDatasetFromFile":
+            return RiDatasetFromFile(config=config)
         case _:
             raise ValueError(
                 f"Dataset type {config.dataset.type} is not supported. It can be implemented in the datasets directory."
