354939d41a322e71959f029d3fd5cd7f09755ea6
Daniël Nobbe <daniellnobbe@gmail.com>
2026-04-10 10:17:16 +0200

    don't drop last during eval if using batched eval
    Signed-off-by: Daniël Nobbe <daniellnobbe@gmail.com>

--- src/ignition/engines.py
+++ src/ignition/engines.py
@@ -347,7 +347,8 @@ def setup_evaluator(
                 val_dataset,
                 num_workers=config.get("num_workers", 1),
                 collate_fn=list_data_collate,
-                batch_size=config.eval_batch_size
+                batch_size=config.eval_batch_size,
+                drop_last=False
             )
             post_transforms = instantiate_post_transforms(
                 config.post_transforms.get(name) if name is not None else config.post_transforms,
